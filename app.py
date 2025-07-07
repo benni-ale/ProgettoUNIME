d740b1bf0f452cc2c3377b07eef7f0a83f0eea20
benni-ale <abeninati98@gmail.com>
2025-07-07 06:11:37 +0200

    ok

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Placeholder for news extraction logic
+    news_data = []  # This will be replaced with actual data
+    return render_template('index.html', news=news_data)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
