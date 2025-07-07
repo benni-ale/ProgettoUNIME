217205ff83b605b71107330c9c2ecd9d81168ac5
benni-ale <abeninati98@gmail.com>
2025-07-07 08:23:42 +0200

    ok

--- app.py
+++ app.py
@@ -45,5 +45,7 @@ def search():
             results.append({'title': art['title'], 'url': url, 'error': str(e)})
     return jsonify(results)
 
+import os
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
